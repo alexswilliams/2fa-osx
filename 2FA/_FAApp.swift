7b25aafbd0f7fd32b464ebf3e8f9459a33b257a8
Alex Williams <alex.williams1@starlingbank.com>
2023-01-15 02:15:20 +0000

    Initial Commit

---
+++ 2FA/_FAApp.swift
@@ -0,0 +1,17 @@
+//
+//  _FAApp.swift
+//  2FA
+//
+//  Created by Alex Simeon Williams on 15/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct _FAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
